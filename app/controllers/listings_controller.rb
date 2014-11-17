eaa073ad60569d6df123823bba981cf25ebe0075
ntorres91 <neil.k.torres@gmail.com>
2014-11-17 17:59:31 -0500

    create owner and renter showpage and edit model listings

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,4 +7,26 @@ class ListingsController < ApplicationController
   def new
     @listing=Listing.new
   end
+
+  def create
+    @listing=Listing.create(listing_params)
+    redirect_to :back
+  end
+
+  def show
+    @listing=Listing.find(params[:id])
+    @user= User.find(@listing.user_id)
+    if current_user==@user
+      render :owner_show
+    else
+      render :renter_show
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :description, :image_url)
+
+  end
 end
