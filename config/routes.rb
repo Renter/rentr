570b5aacc9df9411f3f955e97a47e24147d42663
JohnBuonomo <john.a.buonomo@gmail.com>
2014-11-18 20:15:02 -0500

    adds google maps api and maps controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,21 @@ require "monban/constraints/signed_out"
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+<<<<<<< HEAD
   resources :locations, only: [:show, :create]
   resources :searches, only: [:index]
   resources :listings, only: [:show]
   resources :searches, only: [:index]
+=======
+<<<<<<< HEAD
+
+=======
+  resources :locations, only: [:show, :create]
+  resources :searches, only: [:index]
+  resources :listings, only: [:show]
+  get "/maps" => "maps#show"
+>>>>>>> 38e52c9... added google map api and maps controller to begin to include it.
+>>>>>>> adds google maps api and maps controller
   constraints Monban::Constraints::SignedIn.new do
     root "dashboards#show", as: :dashboard
   end
