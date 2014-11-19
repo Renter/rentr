444f0d1abd17a2370e89b54af36b14c2c051bc20
JohnBuonomo <john.a.buonomo@gmail.com>
2014-11-19 18:29:05 -0500

    adds images that belong to listings, when users make listings it saves it to a city if the name matches

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -9,6 +9,10 @@ class ListingsController < ApplicationController
 
   def create
     @listing = Listing.new(listing_params)
+    
+    if find_location(@listing.city)
+      @listing.location_id = find_location(@listing.city).id
+    end
     if @listing.save
       redirect_to @listing
     else
@@ -29,12 +33,19 @@ class ListingsController < ApplicationController
 
   private
 
+  def find_location(city_name)
+    Location.find_by(name: city_name.downcase) 
+  end
+  
   def listing_params
     params.require(:listing).permit(
       :description,
       :name,
       :user_id,
+<<<<<<< HEAD
       :main_image,
+=======
+>>>>>>> adds images that belong to listings, when users make listings it saves it to a city if the name matches
       :city,
       :street,
       :number
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,7 +7,10 @@ class Listing < ActiveRecord::Base
       thumb: "100x100>" 
     },
     default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\Z/
+  
+  def location_names
+    Location.all.map{|location| location.name}
+  end
 end
 
 
