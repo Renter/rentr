f80102199633eac13a0cb5aada44e614743f16ce
JohnBuonomo <john.a.buonomo@gmail.com>
2014-11-19 17:58:03 -0500

    adds more columns to listings, removes image url from listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20141120154921) do
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +55,7 @@ ActiveRecord::Schema.define(version: 20141120154921) do
     t.integer  "main_image_file_size"
     t.datetime "main_image_updated_at"
     t.string   "full_address"
+
   end
 
   create_table "locations", force: true do |t|
