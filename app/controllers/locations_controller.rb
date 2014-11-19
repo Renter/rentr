3763cc2587f62ece7c80df50d0945ae45d78c4db
JohnBuonomo <john.a.buonomo@gmail.com>
2014-11-19 10:01:13 -0500

    location search returns city show page if exact match, returns search results otherwise

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,11 @@
 class LocationsController < ApplicationController
   def create
     @location = Location.new(location_params)
+<<<<<<< HEAD
+=======
+    @location.name = @location.name.downcase
+    @location.state = @location.state.downcase
+>>>>>>> location search returns city show page if exact match, returns search results otherwise
     if @location.save
       redirect_to root_path
     else
