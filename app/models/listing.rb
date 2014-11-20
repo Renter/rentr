b662cf86fb5d7d3301be9554ab19babb12f0cf91
JohnBuonomo <john.a.buonomo@gmail.com>
2014-11-20 11:08:38 -0500

    again the hound

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,9 @@ class Listing < ActiveRecord::Base
   belongs_to :user
   has_many :date_ranges
 
-  has_attached_file :main_image, styles: { medium: "300x300>", thumb: "100x100>" }, 
+  has_attached_file :main_image, styles: {
+    medium: "300x300>",
+    thumb: "100x100>" }, 
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\Z/
 
